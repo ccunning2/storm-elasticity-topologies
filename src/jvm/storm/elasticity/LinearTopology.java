6ce10b70df89526963f75ed8eb0b49111b702db8
Jerry <jerry@ubuntu.(none)>
2015-02-19 14:52:51 -0800

    testing

--- src/jvm/storm/elasticity/LinearTopology.java
+++ src/jvm/storm/elasticity/LinearTopology.java
@@ -14,7 +14,7 @@ public class LinearTopology {
 
 		TopologyBuilder builder = new TopologyBuilder();
 
-		builder.setSpout("spout_head", new TestSpout(), paralellism);
+		builder.setSpout("spout_head", new TestSpout(), paralellism/1.5);
 
 		for (int i = 0; i < numBolt; i++) {
 			if (i == 0) {
