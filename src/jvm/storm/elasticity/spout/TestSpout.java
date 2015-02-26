b61994ae91d2c7711ad2646de151b2754952d3c9
Jerry <jerry@ubuntu.(none)>
2015-02-25 19:43:34 -0800

    debugging

--- src/jvm/storm/elasticity/spout/TestSpout.java
+++ src/jvm/storm/elasticity/spout/TestSpout.java
@@ -20,7 +20,7 @@ public class TestSpout extends BaseRichSpout {
 
 	  @Override
 	  public void nextTuple() {
-		  Utils.sleep(0.01);
+		  Utils.sleep(1);
 	    _collector.emit(new Values("jerry"));
 	  }
 
