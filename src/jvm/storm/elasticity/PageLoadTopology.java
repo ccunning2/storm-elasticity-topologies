c4c08c8cb6e347bc164a8ea226a71b6b75d0d320
Jerry <jerry@ubuntu.(none)>
2015-04-09 13:47:02 -0700

    testing

--- src/jvm/storm/elasticity/PageLoadTopology.java
+++ src/jvm/storm/elasticity/PageLoadTopology.java
@@ -16,22 +16,22 @@ public class PageLoadTopology {
 
 		TopologyBuilder builder = new TopologyBuilder();
 
-		builder.setSpout("spout_head", new RandomLogSpout(), paralellism).setNumTasks(10);
+		builder.setSpout("spout_head", new RandomLogSpout(), paralellism).setNumTasks(16);
 
 
-		builder.setBolt("bolt_transform", new TransformBolt(), paralellism).shuffleGrouping("spout_head").setNumTasks(10);
-		builder.setBolt("bolt_filter", new FilterBolt(), paralellism).shuffleGrouping("bolt_transform").setNumTasks(10);
-		builder.setBolt("bolt_join", new TestBolt(), paralellism).shuffleGrouping("bolt_filter").setNumTasks(10);
-		builder.setBolt("bolt_filter_2", new FilterBolt(), paralellism).shuffleGrouping("bolt_join").setNumTasks(10);
-		builder.setBolt("bolt_aggregate", new AggregationBolt(), paralellism).shuffleGrouping("bolt_filter_2").setNumTasks(10);
-		builder.setBolt("bolt_output_sink", new TestBolt(),paralellism).shuffleGrouping("bolt_aggregate").setNumTasks(10);
+		builder.setBolt("bolt_transform", new TransformBolt(), paralellism).shuffleGrouping("spout_head").setNumTasks(16);
+		builder.setBolt("bolt_filter", new FilterBolt(), paralellism).shuffleGrouping("bolt_transform").setNumTasks(16);
+		builder.setBolt("bolt_join", new TestBolt(), paralellism).shuffleGrouping("bolt_filter").setNumTasks(16);
+		builder.setBolt("bolt_filter_2", new FilterBolt(), paralellism).shuffleGrouping("bolt_join").setNumTasks(16);
+		builder.setBolt("bolt_aggregate", new AggregationBolt(), paralellism).shuffleGrouping("bolt_filter_2").setNumTasks(16);
+		builder.setBolt("bolt_output_sink", new TestBolt(),paralellism).shuffleGrouping("bolt_aggregate").setNumTasks(16);
 
 		Config conf = new Config();
 		conf.setDebug(true);
 
 		conf.setNumAckers(0);
 
-		conf.setNumWorkers(12);
+		conf.setNumWorkers(16);
 
 		StormSubmitter.submitTopologyWithProgressBar(args[0], conf,
 				builder.createTopology());
